fa546c6434deec5aa794e23862f45c88bc2a1fa1
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-19 00:15:48 +0530

    Update Email.java

--- edu/univ/erp/util/Email.java
+++ edu/univ/erp/util/Email.java
@@ -1,10 +1,29 @@
-package edu.univ;
+package edu.univ.erp.util;
+import exceptions.*;
 
-public class Email{
-  private String username;
-  private String domain;
+/**
+getUsername : to get username from email address
+getDomain : to get domain from email address
 
-  public boolean isCorrect(String email){
-    if (email.contains("@"))
+also checks for validity of email, throws InvalidEmailException if not @ found in the email address
+*/
+
+public final class Email{
+  public static String getUsername(String email) throws InvalidEmailException{
+    if (email.contains("@")){
+      return email.split("@")[0];
+    }
+    else{
+      throw new InvalidEmailException();
+    }
+  }
+
+  public static String getDomain(String email) throws InvalidEmailException{
+    if (email.contains("@")){
+      return email.split("@")[1];
+    }
+    else{
+      throw new InvalidEmailException();
+    }
   }
 }
