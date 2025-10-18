52000c3a4cc9ed00a4a28f0fa77be1a59215aabb
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-18 23:51:38 +0530

    Update InvalidEmailException.java

--- edu/univ/erp/exceptions/InvalidEmailException.java
+++ edu/univ/erp/exceptions/InvalidEmailException.java
@@ -1,7 +1,12 @@
 package edu.univ.erp.exceptions;
-
 import java.lang.Exceptions;
 
+/**
+Thrown when email address is not valid.
+E.g. 
+chaitanya24158iiitd.ac.in (missing @)
+*/
+
 public class InvalidEmailException extends Exception{
-  
+  public Exception(){};
 }
