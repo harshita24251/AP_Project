7c8bb665f932cb731a44234409c741b33063031b
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-20 14:47:00 +0530

    Update InvalidLoginException.java

--- edu/univ/erp/exceptions/InvalidLoginException.java
+++ edu/univ/erp/exceptions/InvalidLoginException.java
@@ -1,10 +1,10 @@
 package edu.univ.erp.exceptions;
-import java.lang.Exceptions;
+import java.lang.Exception;
 
 /**
 Thrown when the user adds wrong password for a username in auth_DB.
 */
 
-public class InvalidLoginException extends Exceptions{
+public class InvalidLoginException extends Exception{
   Exception();
 }
