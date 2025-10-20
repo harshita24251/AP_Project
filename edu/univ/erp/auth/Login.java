f001a63b7003a51ffd0734c4f36c3acfbdbe628d
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-20 14:56:34 +0530

    Update and rename temp to Login.java

---
+++ edu/univ/erp/auth/Login.java
@@ -0,0 +1,10 @@
+package edu.univ.erp.auth;
+import exceptions.*;
+import java.sql.*;
+
+public class Login{
+  public static boolean isValid() throws InvalidLoginException, UserAccountNotFoundException{
+    //Connection connect = DriverManager.getConnection("jdbc:h2:../../../../db", "iiitd", "student");
+    
+  }
+}
