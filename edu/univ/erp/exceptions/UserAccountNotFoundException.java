c4d86b744d7105b840837321afc3ea1b74994a87
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-20 14:46:24 +0530

    Create UserAccountNotFoundException.java

---
+++ edu/univ/erp/exceptions/UserAccountNotFoundException.java
@@ -0,0 +1,10 @@
+package edu.univ.erp.exceptions;
+import java.lang.Exception;
+
+/**
+Thrown when the username entered by the user is not found in the database.
+*/
+
+public class UserAccountNotFoundException extends Exception{
+  Exception();
+}
