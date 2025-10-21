b074c11f4a4727bcf1319477ea0da3456a4a0168
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-21 22:31:36 +0530

    edited HikariConnectionPool.java

--- edu/univ/erp/auth/HikariConnectionPool.java
+++ edu/univ/erp/auth/HikariConnectionPool.java
@@ -3,13 +3,26 @@ package edu.univ.erp.auth;
 import java.sql.*;
 import com.zaxxer.hikari.*;
 
+/**
+This is a class that provides a datasource from a connection pool managed by HikariCP.
+The getDataSource method returns a DataSource object from the connection pool.
+The static initialisation block is executed after the class is loaded.
+
+This can significantly reduce the time required to repeatedly connect to the database to read and write data.
+*/
+
 public class HikariConnectionPool(){
-  public static DataSource getDataSource(){
+  public static final HikariDataSource sourceOfData;
+  
+  static {
     HikariConfig conf = new HikariConfig();
     conf.setJdbcUrl("jdbc:h2:../../../../db/Auth_DB");
     conf.setUsername("iiitd");
     conf.password("student");
+    sourceOfData =  new HikariDataSource(conf);
+  }
 
-    return new HikariDataSource(conf);
-  }  
+  public static DataSource getDataSource(){
+    return sourceOfData;
+  }
 }
