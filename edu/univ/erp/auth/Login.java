3285eb5de2f9747c3bdec4348816aad959ca583c
Chaitanya Satsangi <chaitanyasatsangi26@gmail.com>
2025-10-21 21:37:03 +0530

    Update Login.java

--- edu/univ/erp/auth/Login.java
+++ edu/univ/erp/auth/Login.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Login{
   public static boolean isValid() throws InvalidLoginException, UserAccountNotFoundException{
-    //Connection connect = DriverManager.getConnection("jdbc:h2:../../../../db", "iiitd", "student");
+    Connection connect = HikariConnectionPool.getDataSource.getConnection();
     
   }
 }
