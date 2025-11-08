a4d1bf58b138fbd50196ad4dfabf174fae9b2dc2
0xChaitanya <chaitanyasatsangi26@gmail.com>
2025-11-08 15:38:54 +0530

    started student dashboard

---
+++ src/edu/univ/erp/ui/student/Dashboard.java
@@ -0,0 +1,59 @@
+import edu.univ.erp.ui.assets.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.Image.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+import com.formdev.flatlaf.*;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import java.imageio.*;
+import java.lang.Exception;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Dashboard extends JFrame{
+    public Dashboard(){
+         
+    }
+}
+
+class LeftNavPanel extends JPanel{
+    public LeftNavPanel(String panelTitle, String[] selectables, int width, int height, int fontSize){
+        this.setPreferredSize(new Dimension(width, height));
+        //-----------------------Loading Font--------------------
+        GraphicsEnvironment ge;
+        try{
+            ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Roboto-Light.ttf")));
+        }
+        catch (IOException | FontFormatException e){}
+
+        try{
+            title.setFont(new Font("Roboto Light", Font.PLAIN, 16));
+        }
+        catch (NullPointerException e){}
+        //========================================================
+        JPanel titlePanel = new JPanel();
+        titlePanel.setPreferredSize(new Dimension(width, 30));
+        JPanel btnPanel = new JPanel();
+        btnPanel.setPreferredSize(new Dimension(width, height - 30));
+        //-------------------------Icons--------------------------
+        Icon bullet = new FlatSVGIcon("bullet.svg", 0.25f);
+
+        //-------------------------Labels-------------------------
+        JLabel title = new JLabel(panelTitle);
+        title.setFont("Roboto Mono", Font.PLAIN, fontSize-4);
+
+        //-----------------------Adding Labels---------------------
+        titlePanel.add(title);
+
+        for (String s : selectables){
+            JLabel putLabel = new JLabel(s, bullet, JLabel.LEFT);
+            putLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            putLabel.setFont("Roboto Mono", Font.PLAIN, 16);
+            btnPanel.add(putLabel);
+            btnPanel.add(Box.createRigidArea(new Dimension(10, 10)));
+        }
+    }
+}
