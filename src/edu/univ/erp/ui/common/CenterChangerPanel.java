97bc0ed24073a2a948b66273cdf0c7b72248d039
0xChaitanya <chaitanyasatsangi26@gmail.com>
2025-11-11 09:57:19 +0530

    center panel added

---
+++ src/edu/univ/erp/ui/common/CenterChangerPanel.java
@@ -0,0 +1,15 @@
+package edu.univ.erp.ui.common;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * this panel appears in the middle of the window and helps change page
+ */
+
+public class CenterChangerPanel() extends JPanel{
+    public CenterChangerPanel(float width, float height){
+        setPreferredSize(new Dimension(Math.round(width), Math.round(height)));
+        setLayout(new BorderLayout());
+    }
+}
